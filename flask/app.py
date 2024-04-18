c94e388148100d132576470fcfdfd47c2c09456e
nass <nassime.lahbib@univ-rouen.fr>
2024-04-18 12:43:01 +0200

    V1

---
+++ flask/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request, redirect
+from flask_mysqldb import MySQL
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+
+#infos DB
+app.config['MYSQL_HOST'] = '172.19.0.3'
+app.config['MYSQL_USER'] = 'sae61'
+app.config['MYSQL_PASSWORD'] = 'sae61'
+app.config['MYSQL_DB'] = 'sae61'
+
+mysql = MySQL(app)
+
+@app.route("/")
+def accueil():
+    return render_template('accueil.html')
+
+@app.route("/inscription", methods=['GET', 'POST'])
+def login():
+	if request.method == 'POST':
+		Users = request.form
+		Nom = Users['nom']
+		Prenom = Users['prenom']
+		identifiant = Users['identifiant']
+		mail = Users['email']
+		password = Users['mdp']
+		hashed = generate_password_hash(password)
+		cur = mysql.connection.cursor()
+		cur.execute("INSERT INTO Utilisateurs(Nom, Prenom, identifiant, mail, password) VALUES (%s,%s,%s,%s,%s)",{Nom, Prenom, identifiant, mail, hashed})
+		mysql.connection.commit()
+		cur.close()
+		return "Vous êtes inscrit !"
+	return render_template('inscription.html')
+
+"""@app.route("/connexion" methods=['GET', 'POST'])
+def connexion():
+	if request.method == 'POST':
+	
+    return render_template('connexion.html')///"""
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
---
+++ run.sh
@@ -0,0 +1,13 @@
+#Installation des conteneurs Mysql, flask et adminer
+docker compose up -d
+echo "Démarrage des conteneurs..."
+sleep 10
+#Création de la table Utilisateurs
+echo "Récupération de l'IP du conteneur MariaDB et insertion de la table Utilisateurs..."
+
+host=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' mariadb)
+mysql -u root -p'foo' -h $host < ~/SAE61_WEB/SQL/table.sql
+
+echo "OK !"
+
+

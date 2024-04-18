585b3426e70993b04775ccc3fc91b173a55b0a95
nass <nassime.lahbib@univ-rouen.fr>
2024-04-18 16:16:52 +0200

    modification du nb de caractère

--- SQL/table.sql
+++ SQL/table.sql
@@ -1,3 +1,3 @@
 #CREATE DATABASE sae61;
 USE sae61;
-CREATE TABLE Utilisateurs (id int AUTO_INCREMENT, Nom varchar(45) NOT NULL, Prenom varchar(45) NOT NULL, identifiant varchar(45) NOT NULL, mail varchar(45) NOT NULL,password varchar(45) NOT NULL, PRIMARY KEY (id));
+CREATE TABLE Utilisateurs (id int AUTO_INCREMENT, Nom varchar(45) NOT NULL, Prenom varchar(45) NOT NULL, identifiant varchar(45) NOT NULL, mail varchar(45) NOT NULL,password varchar(300) NOT NULL, PRIMARY KEY (id));
